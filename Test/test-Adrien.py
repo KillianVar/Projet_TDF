fe5682f2258666b09462c22dca143069b7d1cc1e
Adri-78 <91499346+Adri-78@users.noreply.github.com>
2022-06-22 15:51:43 +0200

    Hello me

--- Test/test-Adrien.py
+++ Test/test-Adrien.py
@@ -3,6 +3,9 @@ from gedcom.parser import Parser
 from gedcom.element.element import Element
 from gedcom.element.family import FamilyElement
 
+import json
+import tree_manager
+
 
 gedcom_parser = Parser()
 file_path = 'Queen_Eliz_II.ged'
@@ -69,3 +72,55 @@ for element in root_child_elements:
                 f'parents are {parent_name[0][0]} {parent_name[0][1]} and {parent_name[1][0]} {parent_name[1][1]}.'
         except ValueError:
             pass
+
+
+# Ajout des photos comme nouvelles relations
+
+# Description du problème : un individu étant très proche socialement d'un autre individu sans que leur
+# position dans l'arbre soit proche doivent être reconnus par l'algorithme
+
+# Solution proposée : se fonder sur les photos de famille sur lesquelles sont identifiés les individus
+# Plus deux individus ont d'occurences - photo, plus leur distance va diminuer
+
+# Exemple précis : individu qui n'a pas été élevé par ses parents mais par sa tante
+
+
+# entrée : dictionnaire relatif à l'individu {'@I...@' : 'nbre occurences'}
+
+# fonctionnement général : notre petit algorithme prend en entrée le dictionnaire d'occurences, puis vient
+# modifier la distance entre les individus en fonction de cette occurence.
+
+dict_occur_I521 = {"@I521@" : {"@I431@": 5, "@I432@": 3, "@I392@": 1, "@I6456@": 2}}
+
+def photo_occur_detector(dict_occur):
+
+# ouverture de l'ancienne base de données
+
+    file_calculus = open('./Database/database_calculus.json')
+    calculus = json.load(file_calculus)
+    file_relations = open('./Database/database_conversion.json')
+    relations = json.load(file_relations)
+    individual = list(dict_occur.keys())[0]
+    dict_occur_inside = dict_occur.values()
+
+    dict_indi = relations[f'@{individual}@']
+    dict_calculus = calculus[f'@{individual}@']
+
+    for people in dict_occur_inside.keys():
+
+        if people in dict_calculus.keys():
+
+            new_dist = (1/dict_occur_inside[f"{people}"]) * dict_calculus[f"{people}"]
+            dict_calculus[f"{people}"] = new_dist
+
+        else:
+
+            dict_link = dict_indi["links"]
+            dict_link["photo_relation"] = people    # ajout de la nouvelle relation
+
+            dict_calculus[f"{people}"] = 5 * (1/dict_occur_inside[f"{people}"])
+
+    tree_manager.saver_base(relations, 'database_conversion')  # enregistrement en écrasant l'ancienne base
+    tree_manager.saver_base(calculus, 'database_calculus')
+
+photo_occur_detector(dict_occur_I521)
---
+++ photo_occurences.py
@@ -0,0 +1,58 @@
+import json
+import tree_manager
+
+
+# Ajout des photos comme nouvelles relations
+
+# Description du problème : un individu étant très proche socialement d'un autre individu sans que leur
+# position dans l'arbre soit proche doivent être reconnus par l'algorithme
+
+# Solution proposée : se fonder sur les photos de famille sur lesquelles sont identifiés les individus
+# Plus deux individus ont d'occurences - photo, plus leur distance va diminuer
+
+# Exemple précis : individu qui n'a pas été élevé par ses parents mais par sa tante
+
+
+# entrée : dictionnaire relatif à l'individu {'@I...@' : 'nbre occurences'}
+
+# fonctionnement général : notre petit algorithme prend en entrée le dictionnaire d'occurences, puis vient
+# modifier la distance entre les individus en fonction de cette occurence.
+
+dict_occur_I101 = {"@I101@" : {"@I431@": 5, "@I432@": 3, "@I392@": 1, "@I6456@": 2}}
+
+def photo_occur_detector(dict_occur):
+
+# ouverture de l'ancienne base de données
+
+    file_calculus = open('./Database/database_calculus.json')
+    calculus = json.load(file_calculus)
+    file_relations = open('./Database/database_conversion.json')
+    relations = json.load(file_relations)
+    individual = list(dict_occur.keys())[0]
+    dict_occur_inside = list(dict_occur.values())[0]
+
+    dict_indi = relations[f"{individual}"]
+    dict_calculus = calculus[f"{individual}"]
+
+    for people in dict_occur_inside.keys():
+
+        not_relation_count = 1
+
+        if people in dict_calculus.keys():
+
+            new_dist = (1/dict_occur_inside[f"{people}"]) * dict_calculus[f"{people}"]
+            dict_calculus[f"{people}"] = new_dist
+
+        else:
+
+            dict_link = dict_indi["links"]
+            dict_link[f"photo_relation_{not_relation_count}"] = people    # ajout de la nouvelle relation
+
+            dict_calculus[f"{people}"] = 5 * (1/dict_occur_inside[f"{people}"])
+
+            not_relation_count += 1
+
+    tree_manager.saver_base(relations, 'database_conversion')  # enregistrement en écrasant l'ancienne base
+    tree_manager.saver_base(calculus, 'database_calculus')
+
+photo_occur_detector(dict_occur_I101)
