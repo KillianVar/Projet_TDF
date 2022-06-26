525fae9f71d495a11d5750054406fcebc3a20308
KillianVar <91543713+KillianVar@users.noreply.github.com>
2022-06-26 16:49:13 +0200

    Commit : - completed visualisation algorithm

--- Test/test-Killian.py
+++ Test/test-Killian.py
@@ -1,272 +1,62 @@
+### NEW TRIAL
+
+# il faut faire un : pip install networkx
+
+# First networkx library is imported along with matplotlib
+import networkx as nx
+import matplotlib.pyplot as plt
 import json
-from gedcom.element.individual import IndividualElement
-from gedcom.parser import Parser
+from Dijkstra_algo import Graph, dijkstra_algorithm, print_result
 
-gedcom_parser = Parser()
 
-file_path = '../Database/Queen_Eliz_II.ged'
-gedcom_parser.parse_file(file_path, False)
+def visualization(id1, id2):
+    file_calculus = open('../Database/database_calculus.json')
+    init_graph = json.load(file_calculus)
+    file_calculus.close()
 
-# Essai de la construction de la BDD
+    file_relations = open('../Database/database_conversion.json')
+    relations = json.load(file_relations)
+    file_relations.close()
 
-bdd = {}
-root_child_elements = gedcom_parser.get_root_child_elements()
-count_people = 0
+    family_graph = nx.Graph()
 
-# Iterate through all root child elements
-for element in root_child_elements:
+    nodes = list(init_graph.keys())
+    people_graph = Graph(nodes, init_graph)
 
-    if isinstance(element, IndividualElement):
+    previous_nodes, shortest_path = dijkstra_algorithm(graph=people_graph, start_node=id1)
+    list_relations, list_people_related = print_result(previous_nodes, shortest_path, id1, id2, relations=relations)
 
-        count_people += 1
+    # Contruction of the visualization
 
-        # creation of a dictionnary for the Individual
+    for person_id in list_people_related:
 
-        id_person = str(element).split()[1]
-        bdd[f"{id_person}"] = {}
+        related_persons = relations[person_id]['links']
 
-        # Filling the dictionnary of the individual
+        for relation_type, related_person in related_persons.items():
 
-        bdd_i = bdd[f"{id_person}"]
-        name, surname = element.get_name()
-        bdd_i["name"] = name
-        bdd_i["surname"] = surname
-        bdd_i["gender"] = element.get_gender()
+            if 'sibling' not in relation_type:
 
-        # Add links between people
-        # Start by creating the links dictionnary that will contain all links for one person
+                family_graph.add_node(related_person)
+                family_graph.add_edge(person_id, related_person, relation=relation_type)
 
-        bdd_i['links'] = {}
-        links = bdd_i['links']
+    chosen_path = []
+    relations_labels = {}
 
-        # Adding ascending parental links
+    for k in range(len(list_relations)):
+        chosen_path.append((list_people_related[k], list_people_related[k + 1]))
+        relations_labels[(list_people_related[k], list_people_related[k + 1])] = list_relations[k]
 
-        parents = gedcom_parser.get_parents(element)
-        count = 0
+    fig, ax = plt.subplots()
 
-        if len(parents) != 0:
+    positions = nx.kamada_kawai_layout(family_graph)
+    nx.draw_networkx_nodes(family_graph, pos=positions, node_size=30)
+    nx.draw_networkx_edges(family_graph, pos=positions, width=0.5,  edge_color='black')
+    nx.draw_networkx_edges(family_graph, pos=positions, width=2., edgelist=chosen_path,
+            edge_color='red')
+    nx.draw_networkx_edge_labels(family_graph, pos=positions, font_size=3,
+                                 edge_labels=relations_labels, label_pos=0.5, alpha=0.8)
+    nx.draw_networkx_labels(family_graph, pos=positions, font_size=5)
+    plt.savefig('test.png', dpi=500)
+    plt.show(dpi=500)
 
-            while len(parents) > 0:
-                parent = parents[0]
-                parent = str(parent).split()[1]
-                links[f'parent_{count + 1}'] = parent
-                count += 1
-                parents.pop(0)
-
-        # Adding descending parental links
-
-        families = gedcom_parser.get_families(element)
-        count = 0
-
-        if len(families) > 0:
-
-            for family in families:
-
-                elements = family.get_child_elements()
-
-                for family_members in family.get_child_elements():
-
-                    if family_members.get_tag() == 'CHIL':
-
-                        child = str(family_members).split()[2]
-
-                        if child not in links.values():
-
-                            links[f'child_{count + 1}'] = child
-                            count += 1
-
-                        if str(family_members).split()[2] not in bdd.keys():
-
-                            bdd[str(family_members).split()[2]] = {'name': None, 'links': {}}
-
-# Adding secondary links
-
-# Adding siblings :
-
-for person, person_data in bdd.items():
-
-    sibling_count = 1
-
-    if 'parent_1' in list(person_data['links'].keys()):
-
-        parent_1 = person_data['links']['parent_1']
-
-        for relation_type, related_person in bdd[parent_1]['links'].items():
-
-            checker = ('child' in relation_type) and not ('grandchild' in relation_type)
-            checker = checker and related_person is not person
-
-            if checker:
-
-                bdd[person]['links'][f'sibling_{sibling_count}'] = related_person
-                sibling_count += 1
-
-
-
-
-# Starting by grandparents
-
-for person, person_data in bdd.items():
-
-    links = person_data['links']
-    links_copy = person_data['links'].copy()
-    count_grandparent = 0
-
-    for link_type, linked_person in links_copy.items():
-
-        if 'parent' in link_type:
-
-            parents = bdd[linked_person]['links']
-
-            for secondary_linked_type, secondary_linked_person in parents.items():
-
-                checker = 'parent' in secondary_linked_type and secondary_linked_person not in links.keys()
-                checker = checker and 'grand' not in secondary_linked_type
-
-                if checker:
-
-                    links[f'grandparent_{count_grandparent + 1}'] = secondary_linked_person
-
-                    count_grandparent += 1
-
-
-# adding grandchildren
-
-for grandchildren, grandchildren_data in bdd.items():
-
-    links = grandchildren_data['links']
-    links_copy = links.copy()
-
-    for link_type, linked_person in links_copy.items():
-
-        if 'grandparent' in link_type:
-
-            grandparent_links = bdd[linked_person]['links']
-
-            n_grand_children = 0
-
-            for relation_grandchildren in grandparent_links.keys():
-
-                if 'grandchildren' in relation_grandchildren :
-
-                    n_grand_children += 1
-
-            grandparent_links[f'grandchildren_{n_grand_children + 1}'] = grandchildren
-
-
-# Adding aunts / uncles :
-
-for grandparent, grandparent_data in bdd.items():
-
-    grandparent_links = grandparent_data['links']
-    links_copy = grandparent_links.copy()
-
-    for link_type, linked_person in links_copy.items():
-
-        if 'grandchildren' in link_type:
-
-            grandchildren = linked_person
-
-            aunt_counter = 1
-
-            # let's count the uncles and aunts the grandchild already has :
-
-            for relation_type, related_person in grandparent_links.items():
-
-                if 'aunt_uncle' in relation_type:
-
-                    aunt_counter += 1
-
-            # let's add to the grandchild new uncles and aunts :
-
-            for relation_type, related_person in grandparent_links.items():
-
-                if 'sibling' in relation_type and related_person not in bdd[grandchildren]['links'].values():
-
-                    bdd[grandchildren]['links'][f'aunt_uncle_{aunt_counter}'] = related_person
-
-
-
-# Adding nephews :
-
-
-for nephew, nephew_data in bdd.items():
-
-    nephew_links = nephew_data['links']
-    links_copy = nephew_links.copy()
-
-    for link_type, linked_person in links_copy.items():
-
-        if 'aunt_uncle' in link_type:
-
-            aunt_uncle = linked_person
-            aunt_uncle_links = bdd[aunt_uncle]['links']
-
-            nephew_counter = 1
-
-            # let's count the nephews and nieces the aunt_uncle already has :
-
-            for relation_type, related_person in aunt_uncle_links.items():
-
-                if 'nephew' in relation_type:
-
-                    nephew_counter += 1
-
-            if not nephew in list(aunt_uncle_links.values()):
-
-                aunt_uncle_links[f'nephew_{nephew_counter}'] = nephew
-
-
-
-# ---------- #
-# parameters for the calculus graph
-
-alpha = 1.
-beta = 1.5
-gamma = 2.
-
-# bdd_calculus_intermediate
-
-bdd_calculus_intermediate = {}
-
-for key, value in bdd.items():
-
-    bdd_calculus_intermediate[key] = {}
-    links = bdd_calculus_intermediate[key]
-
-    for relation_type, linked_person in value['links'].items():
-
-        checker_1 = 'parent' in relation_type or 'child' in relation_type
-        checker_1 = checker_1 and not 'grand' in relation_type
-        checker_2 = 'grandparent' in relation_type or 'grandchild' in relation_type
-        checker_3 = 'sibling' in relation_type
-        checker_4 = 'aunt_uncle' in relation_type
-
-        if checker_1 or checker_3:
-
-            links[linked_person] = alpha
-
-        elif checker_2:
-
-            links[linked_person] = beta
-
-        elif checker_4:
-
-            links[linked_person] = gamma
-
-
-def saver_base():
-
-    path_dbb_content = "../Database/database_conversion.json"
-    python_file = open(path_dbb_content, "w+")
-    json.dump(bdd, python_file)
-    python_file.close()
-
-    path_dbb_calculus_intermediate = "../Database/database_calculus.json"
-    bdd_calculus_file = open(path_dbb_calculus_intermediate, "w+")
-    json.dump(bdd_calculus_intermediate, bdd_calculus_file)
-    bdd_calculus_file.close()
-
-
-saver_base()
-
+visualization("@I392@", "@I406@")
--- visualization.py
+++ visualization.py
@@ -1,76 +1,62 @@
-### NEW TRIAL 
+### NEW TRIAL
 
-#il faut faire un : pip install networkx
+# il faut faire un : pip install networkx
 
 # First networkx library is imported along with matplotlib
 import networkx as nx
 import matplotlib.pyplot as plt
-   
-# Defining a Class
-class GraphVisualization:
-
-    def __init__(self):
-        # visual is a list which stores all the set of edges that constitutes a graph
-        self.visual = []
-          
-    # addEdge function inputs the vertices of an edge and appends it to the visual list
-    def addEdge(self, a, b):
-        temp = [a, b]
-        self.visual.append(temp)
-          
-    # In visualize function G is an object of
-    # class Graph given by networkx G.add_edges_from(visual)
-    # creates a graph with a given list
-    # nx.draw_networkx(G) - plots the graph
-    # plt.show() - displays the graph
-    def visualize(self):
-        G = nx.Graph()
-        G.add_edges_from(self.visual)
-        nx.draw_networkx(G)
-        plt.show()
-  
-# Driver code
-# We're going to use the Dijstra_algo.py file to get the relations between two persons
-import sys
 import json
-from gedcom.element.individual import IndividualElement
-from gedcom.parser import Parser
 from Dijkstra_algo import Graph, dijkstra_algorithm, print_result
 
 
-def visualization (id1, id2):
-    
-    file_calculus = open('Database/database_calculus.json')
+def visualization(id1, id2):
+    file_calculus = open('../Database/database_calculus.json')
     init_graph = json.load(file_calculus)
     file_calculus.close()
 
-    file_relations = open('Database/database_conversion.json')
+    file_relations = open('../Database/database_conversion.json')
     relations = json.load(file_relations)
     file_relations.close()
 
-    name1, surname1 = relations[id1]["name"], relations[id1]["surname"]
+    family_graph = nx.Graph()
 
     nodes = list(init_graph.keys())
     people_graph = Graph(nodes, init_graph)
 
     previous_nodes, shortest_path = dijkstra_algorithm(graph=people_graph, start_node=id1)
-    list_relations, list_people_related = print_result(previous_nodes, shortest_path, id1, id2, relations=relations) #create two lists : one with the relations that connect the two people and one with the ids of the people in between 
+    list_relations, list_people_related = print_result(previous_nodes, shortest_path, id1, id2, relations=relations)
 
+    # Contruction of the visualization
 
-    # Contruction of the visualization 
-    G = GraphVisualization()
-    G.addEdge(f"origin : {id1}", f"{list_relations[0]}, ':', {list_people_related[0]}" )
-    for i in range(0, len(list_relations)-1):
-        G.addEdge(f"{list_relations[i]}, ':', {list_people_related[i]}", f"{list_relations[i+1]}, ':', {list_people_related[i+1]}")
-    G.visualize()
+    for person_id in list_people_related:
 
-    """ ou si on veut afficher les noms
-    G = GraphVisualization()
-    namex, surnamex = relations[list_people_related[0]]["name"], relations[list_people_related[1]]["surname"]
-    G.addEdge(f"{name1} {surname1}", f"{list_relations[0]}, ':', {namex} {surnamex}" )
-    for i in range(1, len(list_relations)-1):
-        G.addEdge(f"{list_relations[i]}, ':', {relations[list_people_related[i]]["name"]} {relations[list_people_related[i]]["surname"]}", f"{list_relations[i+1]}, ':',  {relations[list_people_related[i+1]]["name"]} {relations[list_people_related[i+1]]["surname"]}")
-    G.visualize()
-    """
+        related_persons = relations[person_id]['links']
 
-print(visualization("@I398@", "@I406@"))
+        for relation_type, related_person in related_persons.items():
+
+            if 'sibling' not in relation_type:
+
+                family_graph.add_node(related_person)
+                family_graph.add_edge(person_id, related_person, relation=relation_type)
+
+    chosen_path = []
+    relations_labels = {}
+
+    for k in range(len(list_relations)):
+        chosen_path.append((list_people_related[k], list_people_related[k + 1]))
+        relations_labels[(list_people_related[k], list_people_related[k + 1])] = list_relations[k]
+
+    fig, ax = plt.subplots()
+
+    positions = nx.kamada_kawai_layout(family_graph)
+    nx.draw_networkx_nodes(family_graph, pos=positions, node_size=30)
+    nx.draw_networkx_edges(family_graph, pos=positions, width=0.5,  edge_color='black')
+    nx.draw_networkx_edges(family_graph, pos=positions, width=2., edgelist=chosen_path,
+            edge_color='red')
+    nx.draw_networkx_edge_labels(family_graph, pos=positions, font_size=3,
+                                 edge_labels=relations_labels, label_pos=0.5, alpha=0.8)
+    nx.draw_networkx_labels(family_graph, pos=positions, font_size=5)
+    plt.savefig('test.png', dpi=500)
+    plt.show(dpi=500)
+
+visualization("@I392@", "@I406@")
