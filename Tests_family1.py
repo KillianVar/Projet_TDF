591446ead2105e62a59bea18c2efb295efc0dfe8
21Vania <vania.clesca@mines-paristech.fr>
2022-07-07 15:15:22 +0200

    Create Tests_family1.py

---
+++ Tests_family1.py
@@ -0,0 +1,7 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from tree_inspector import file_reading, tree_analysis
+
+from visualization import path_visualization
+
+path_visualization('database_family1', "@I22@", "@I9@")
